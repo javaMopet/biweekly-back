0b6db1fa9dcbb49d2fa6c69dcaa8b1c2078dc710
Horacio Peña Mendoza <hpenam.dtic@gmail.com>
2023-04-09 21:48:38 -0600

    Cambios en la presentacion de categorias

--- db/seeds/3000_categorias_seed.rb
+++ db/seeds/3000_categorias_seed.rb
@@ -39,41 +39,41 @@ Categoria.create!(nombre: 'Netflix', icono: 'movie', descripcion: 'Netflix',
 Categoria.create!(nombre: 'Spotify', icono: 'fa-brands fa-spotify', descripcion: 'Spotify',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 900)
 
-Categoria.create!(nombre: 'Crédito Taos', icono: 'fa-brands fa-spotify', descripcion: 'Pago mensual de taos',
+Categoria.create!(nombre: 'Crédito Taos', icono: 'directions_car', descripcion: 'Pago mensual de taos',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, cuenta_id: 1, orden: 1000)
-Categoria.create!(nombre: 'Nuevos Proyectos', icono: 'fa-brands fa-spotify', descripcion: 'Nuevos proyectos',
+Categoria.create!(nombre: 'Nuevos Proyectos', icono: 'account_tree', descripcion: 'Nuevos proyectos',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 1100)
-Categoria.create!(nombre: 'Agua', icono: 'fa-brands fa-spotify', descripcion: 'Agua',
+Categoria.create!(nombre: 'Agua', icono: 'emoji_food_beverage', descripcion: 'Agua',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 1200)
-Categoria.create!(nombre: 'Gas', icono: 'fa-brands fa-spotify', descripcion: 'Gas LP',
+Categoria.create!(nombre: 'Gas', icono: 'gas_meter', descripcion: 'Gas LP',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 1300)
-Categoria.create!(nombre: 'CFE', icono: 'fa-brands fa-spotify', descripcion: 'CFE luz eléctrica',
+Categoria.create!(nombre: 'CFE', icono: 'electric_bolt', descripcion: 'CFE luz eléctrica',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 1400)
-Categoria.create!(nombre: 'Dentista', icono: 'fa-brands fa-spotify', descripcion: 'Dentista',
+Categoria.create!(nombre: 'Dentista', icono: 'fa-solid fa-tooth', descripcion: 'Dentista',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 1500)
-Categoria.create!(nombre: 'Tanda', icono: 'fa-brands fa-spotify', descripcion: 'Tanda',
+Categoria.create!(nombre: 'Tanda', icono: 'savings', descripcion: 'Tanda',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 1600)
-Categoria.create!(nombre: 'Gasolina Taos', icono: 'fa-brands fa-spotify', descripcion: 'Gasolina Taos',
+Categoria.create!(nombre: 'Gasolina Taos', icono: 'local_gas_station', descripcion: 'Gasolina Taos',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 1700)
 
-Categoria.create!(nombre: 'Gasolina Lancer', icono: 'fa-brands fa-spotify', descripcion: 'Gasolina Lancer',
+Categoria.create!(nombre: 'Gasolina Lancer', icono: 'local_gas_station', descripcion: 'Gasolina Lancer',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 1800)
-Categoria.create!(nombre: 'Gastos Médicos', icono: 'fa-brands fa-spotify',
+Categoria.create!(nombre: 'Gastos Médicos', icono: 'medical_services',
                   descripcion: 'Médico, psicólogo, oftalmólogo', color: '#1ad560', tipo_movimiento_id: 2,
                   cuenta_contable_id: 511_004, orden: 1900)
-Categoria.create!(nombre: 'Farmacia', icono: 'fa-brands fa-spotify', descripcion: 'Insumos de farmacia',
+Categoria.create!(nombre: 'Farmacia', icono: 'medication', descripcion: 'Insumos de farmacia',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 2000)
-Categoria.create!(nombre: 'Comidas', icono: 'fa-brands fa-spotify', descripcion: 'Comidas',
+Categoria.create!(nombre: 'Comidas', icono: 'fastfood', descripcion: 'Comidas',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 2100)
-Categoria.create!(nombre: 'Comidas efectivo', icono: 'fa-brands fa-spotify', descripcion: 'Comidas pagadas en efectivo',
+Categoria.create!(nombre: 'Comidas efectivo', icono: 'local_dining', descripcion: 'Comidas pagadas en efectivo',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 2200)
-Categoria.create!(nombre: 'Despensa', icono: 'fa-brands fa-spotify', descripcion: 'Despensa pagada sin vales',
+Categoria.create!(nombre: 'Despensa', icono: 'shopping_cart', descripcion: 'Despensa pagada sin vales',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 2300)
 Categoria.create!(nombre: 'Mary Kay', icono: 'fa-brands fa-spotify', descripcion: 'Mary Kay',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 2400)
 Categoria.create!(nombre: 'Just', icono: 'fa-brands fa-spotify', descripcion: 'Just',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 2500)
-Categoria.create!(nombre: 'Mobiliario y equipo', icono: 'fa-brands fa-spotify',
+Categoria.create!(nombre: 'Mobiliario y equipo', icono: 'chair',
                   descripcion: 'Muebles, computadoras, pantallas, teclados, ', color: '#1ad560', tipo_movimiento_id: 2,
                   cuenta_contable_id: 511_004, orden: 2600)
 
@@ -82,50 +82,50 @@ Categoria.create!(nombre: 'Mobiliario y equipo', icono: 'fa-brands fa-spotify',
 #                   cuenta_contable_id: 511_004, orden: 2700)
 Categoria.create!(nombre: 'Anualidades', icono: 'fa-brands fa-spotify', descripcion: 'Anualidades, ',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 2800)
-Categoria.create!(nombre: 'Crédito hipotecario', icono: 'fa-brands fa-spotify', descripcion: 'Hipoteca Scotiabank',
+Categoria.create!(nombre: 'Crédito hipotecario', icono: 'real_estate_agent', descripcion: 'Hipoteca Scotiabank',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 2900)
-Categoria.create!(nombre: 'Adecuaciones Casa', icono: 'fa-brands fa-spotify', descripcion: 'Adecuaciones casa',
+Categoria.create!(nombre: 'Adecuaciones Casa', icono: 'plumbing', descripcion: 'Adecuaciones casa',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 3000)
 
 # Categoria.create!(nombre: 'Adecuaciones Casa', icono: 'fa-brands fa-spotify', descripcion: 'Adecuaciones casa',
 #                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 3100)
-Categoria.create!(nombre: 'Mantenimiento Casa', icono: 'fa-brands fa-spotify', descripcion: 'Mantenimiento casa',
+Categoria.create!(nombre: 'Mantenimiento Casa', icono: 'imagesearch_roller', descripcion: 'Mantenimiento casa',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 3200)
-Categoria.create!(nombre: 'Mantenimiento Auto', icono: 'fa-brands fa-spotify', descripcion: 'Mantenimiento auto',
+Categoria.create!(nombre: 'Mantenimiento Auto', icono: 'car_repair', descripcion: 'Mantenimiento auto',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 3300)
-Categoria.create!(nombre: 'Accesorios Casa', icono: 'fa-brands fa-spotify', descripcion: 'Accesorios casa',
+Categoria.create!(nombre: 'Accesorios Casa', icono: 'bathtub', descripcion: 'Accesorios casa',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 3400)
-Categoria.create!(nombre: 'Ropa, calzado y accesorios', icono: 'fa-brands fa-spotify',
+Categoria.create!(nombre: 'Ropa, calzado y accesorios', icono: 'dry_cleaning',
                   descripcion: 'Ropa, calzado y accesorios', color: '#1ad560', tipo_movimiento_id: 2,
                   cuenta_contable_id: 511_004, orden: 3500)
-Categoria.create!(nombre: 'Trámites', icono: 'fa-brands fa-spotify', descripcion: 'Trámites',
+Categoria.create!(nombre: 'Trámites', icono: 'fa-solid fa-passport', descripcion: 'Trámites',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 3600)
-Categoria.create!(nombre: 'Cursos', icono: 'fa-brands fa-spotify', descripcion: 'Cursos',
+Categoria.create!(nombre: 'Cursos', icono: 'cast_for_education', descripcion: 'Cursos',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 3700)
 
-Categoria.create!(nombre: 'Vinos', icono: 'fa-brands fa-spotify', descripcion: 'Vinos y licores, promociones',
+Categoria.create!(nombre: 'Vinos', icono: 'liquor', descripcion: 'Vinos y licores, promociones',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 3800)
-Categoria.create!(nombre: 'Regalos y padrinos', icono: 'fa-brands fa-spotify', descripcion: 'Regalos y padrinos',
+Categoria.create!(nombre: 'Regalos y padrinos', icono: 'redeem', descripcion: 'Regalos y padrinos',
                   color: '#1ad560', tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 3900)
-Categoria.create!(nombre: 'Vacaciones / Días de campo', icono: 'fa-brands fa-spotify',
+Categoria.create!(nombre: 'Vacaciones / Días de campo', icono: 'airplanemode_active',
                   descripcion: 'Vacaciones y días de campo', color: '#1ad560', tipo_movimiento_id: 2,
                   cuenta_contable_id: 511_004, orden: 4000)
-Categoria.create!(nombre: 'Gastos de fin de semana', icono: 'fa-brands fa-spotify',
+Categoria.create!(nombre: 'Gastos de fin de semana', icono: 'dinner_dining',
                   descripcion: 'Gasto de fin de semana', color: '#1ad560', tipo_movimiento_id: 2,
                   cuenta_contable_id: 511_004, orden: 4100)
-Categoria.create!(nombre: 'Papelería y artículos de oficina', icono: 'fa-brands fa-spotify',
+Categoria.create!(nombre: 'Papelería y artículos de oficina', icono: 'design_services',
                   descripcion: 'Papelería y artículos de oficina', color: '#1ad560', tipo_movimiento_id: 2,
                   cuenta_contable_id: 511_004, orden: 4200)
-Categoria.create!(nombre: 'Otros gastos', icono: 'fa-brands fa-spotify',
+Categoria.create!(nombre: 'Otros gastos', icono: 'icecream',
                   descripcion: 'Otros gastos, gasto por comprobar', color: '#1ad560', tipo_movimiento_id: 2,
                   cuenta_contable_id: 511_004, orden: 4300)
 
-Categoria.create!(nombre: 'Sypris reembolsos', icono: 'fa-brands fa-spotify',
+Categoria.create!(nombre: 'Sypris reembolsos', icono: 'compare_arrows',
                   descripcion: 'Egreso registrado para reembolso de movimientos de sypris', color: '#1ad560',
                   tipo_movimiento_id: 2, cuenta_contable_id: 511_004, orden: 4400)
-Categoria.create!(nombre: 'Devoluciones', icono: 'fa-brands fa-spotify',
+Categoria.create!(nombre: 'Devoluciones', icono: 'compare_arrows',
                   descripcion: 'Devoluciones', color: '#1ad560', tipo_movimiento_id: 2,
                   cuenta_contable_id: 511_004, orden: 4500)
-Categoria.create!(nombre: 'Retiro para inversion/ahorro', icono: 'fa-brands fa-spotify',
+Categoria.create!(nombre: 'Retiro para inversion/ahorro', icono: 'attach_money',
                   descripcion: 'Retiro para inversión o ahorro', color: '#1ad560', tipo_movimiento_id: 2,
                   cuenta_contable_id: 511_004, orden: 4600)
