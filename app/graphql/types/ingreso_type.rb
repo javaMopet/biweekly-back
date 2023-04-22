ecc1f5cf2349692de395782f545252499bf2bc61
Horacio Peña Mendoza <hpenam@uaemex.mx>
2023-04-21 20:26:51 -0600

    cambios

--- app/graphql/types/ingreso_type.rb
+++ app/graphql/types/ingreso_type.rb
@@ -5,6 +5,7 @@ module Types
     field :id, ID, null: false
     field :categoria_id, Integer, null: false
     field :cuenta_id, Integer, null: false
+    field :cuenta, Types::Cuentas::CuentaType, null: false
     field :observaciones, String
     field :registro, Types::RegistroType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
