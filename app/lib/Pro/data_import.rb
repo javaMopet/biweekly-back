b3aaeb1c0b43acabff8666762c65b25f9a4f2e28
Horacio Peña Mendoza <hpenam@uaemex.mx>
2023-05-11 19:17:13 -0600

    cambios

--- app/lib/Pro/data_import.rb
+++ app/lib/Pro/data_import.rb
@@ -3,9 +3,8 @@
 module Pro
   # Clase para la importacion de datos por procedimiento almacenado.
   class DataImport < ActiveRecord::Base
-    def self.buscar_columnas(ejercicio_fiscal)
-      p " Buscando columnas con ejercicio_fiscal: #{ejercicio_fiscal}"
-      execute_procedure('PA_COLUMNAS', 2023, 4, 1, 2)
+    def self.buscar_columnas(ejercicio_fiscal, mes)      
+      execute_procedure('PA_COLUMNAS', ejercicio_fiscal, mes, 1, 2)
     end
 
     def self.buscar_movimientos(_ejercicio_fiscal, tipo_movimiento_id, is_saldos)
