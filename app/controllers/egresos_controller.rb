05d940b9beedeed9b3d2fe0e30fc2aff6fb994de
Horacio Peña Mendoza <hpenam.dtic@gmail.com>
2023-06-05 23:08:44 -0600

    Cambios

--- app/controllers/egresos_controller.rb
+++ app/controllers/egresos_controller.rb
@@ -27,8 +27,29 @@ class EgresosController < ApplicationController
   # POST /create_list
   def create_list
     listado = params.fetch(:lista_egresos, [])
-    listado_json =  JSON.parse(lista_egresos.to_json)
-    p (listado_json.to_a)[0]["id"]
+    # p listado.class
+    # p listado
+    # listado = params["lista_egresos"]
+    # listado_json =  JSON.parse(listado.to_json)
+    # p listado_json.class
+    # p listado_json
+    # p (listado_json.to_a)[0]["registro"]
+    listado.each do |egreso|
+      p egreso["registro"]
+      p egreso[:registro]
+      p egreso[:categoria_id]
+      p egreso[:cuenta_id]
+      p egreso[:registro][:estado_registro_id]
+      registro = obtener_registro egreso[:registro]
+      egreso_save = obtener_egreso egreso
+
+      registro.registrable = egreso_save
+      egreso_save.registro = registro
+
+      unless egreso_save.save
+        p egreso_save.errors.full_messages
+      end       
+    end
   end
 
   # PATCH/PUT /egresos/1
@@ -54,6 +75,25 @@ class EgresosController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def egreso_params
-    params.require(:egreso).permit(:categoria_id, :cuenta_id, :observaciones)
+    params.require(:egreso).permit(:categoria_id, :cuenta_id, :observaciones, registro: [:estado_registro_id, :importe, :fecha, :observaiones])
+  end
+
+  def obtener_registro(registro_aux)
+    registro = Registro.new
+
+    registro.estado_registro_id = registro_aux[:estado_registro_id]
+    registro.importe = registro_aux[:importe]
+    registro.fecha = registro_aux[:fecha]
+    registro.observaciones = registro_aux[:observaciones]
+
+    registro
+  end
+
+  def obtener_egreso(egreso_aux)
+    egreso =Egreso.new
+    egreso.categoria_id = egreso_aux[:categoria_id]
+    egreso.cuenta_id = egreso_aux[:cuenta_id]
+
+    egreso
   end
 end
