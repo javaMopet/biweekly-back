65ed758bd2a5bf9a39b6098a5051b10f14994925
Horacio Peña Mendoza <hpenam@uaemex.mx>
2023-06-21 16:55:38 -0600

    avance

--- app/graphql/types/ingreso_type.rb
+++ app/graphql/types/ingreso_type.rb
@@ -3,8 +3,7 @@
 module Types
   class IngresoType < Types::BaseObject
     field :id, ID, null: false
-    field :categoria_id, Integer, null: false
-    field :observaciones, String
+    field :categoria_id, Integer, null: false    
     field :registro, Types::RegistroType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
