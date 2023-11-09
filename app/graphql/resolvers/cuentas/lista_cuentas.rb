ef7686592eb4f2c456f77005a069243070a6ae85
Horacio Peña Mendoza <hpenam.dtic@gmail.com>
2023-11-08 18:52:17 -0600

    cambios

--- app/graphql/resolvers/cuentas/lista_cuentas.rb
+++ app/graphql/resolvers/cuentas/lista_cuentas.rb
@@ -6,6 +6,7 @@ module Resolvers
     class ListaCuentas < Resolvers::Base
       type [Types::Cuentas::CuentaType], null: false
 
+      # Default method
       def resolve
         Cuenta.includes(:cuenta_contable, :tipo_cuenta, :banco).all
       end
