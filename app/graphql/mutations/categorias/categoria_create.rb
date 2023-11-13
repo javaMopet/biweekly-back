34aba2cdb2d049a8b96d75ae37b15032b4ef36eb
Horacio Peña Mendoza <hpenam.dtic@gmail.com>
2023-11-12 23:31:53 -0600

    cambios

--- app/graphql/mutations/categorias/categoria_create.rb
+++ app/graphql/mutations/categorias/categoria_create.rb
@@ -2,7 +2,7 @@
 
 module Mutations
   module Categorias
-    # Creación de categorias
+    # Creacion de categorias
     class CategoriaCreate < BaseMutation
       description 'Creates a new categoria'
 
@@ -10,12 +10,11 @@ module Mutations
 
       argument :categoria_input, Types::Categorias::CategoriaInputType, required: true
 
+      # Default method
       def resolve(categoria_input:)
         categoria = ::Categoria.new(**categoria_input)
         unless categoria.save
-          raise GraphQL::ExecutionError
-            .new "Error creating categoria #{categoria.errors.full_messages}",
-                 extensions: categoria.errors.to_hash
+          raise GraphQL::ExecutionError.new "Error creating categoria", extensions: categoria.errors.to_hash
         end
 
         { categoria: }
