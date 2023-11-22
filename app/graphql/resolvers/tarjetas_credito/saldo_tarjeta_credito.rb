a318c2dbcfee409ca1f4c41f9f319704dd738291
Horacio Peña Mendoza <hpenam.dtic@gmail.com>
2023-11-21 22:53:28 -0600

    cambios

--- app/graphql/resolvers/tarjetas_credito/saldo_tarjeta_credito.rb
+++ app/graphql/resolvers/tarjetas_credito/saldo_tarjeta_credito.rb
@@ -12,18 +12,16 @@ module Resolvers
 
       # default method
       def resolve(cuenta_id:, fecha_fin:, is_detalle:)
-        fecha_final = nil
-
-        if fecha_fin.nil?
-          fecha_final = Time.current.to_date
-
-          p fecha_final
-        else
-          fecha_final = fecha_fin
-        end
+        fecha_final =
+          if fecha_fin.nil?
+            Time.current.to_date
+          else
+            fecha_fin
+          end
 
         saldo_result = Pro::DataImport.saldo_tarjeta_credito(fecha_final, cuenta_id, is_detalle).first
-        saldo = saldo_result["saldo"]
+
+        saldo = saldo_result["saldo"].nil? ? 0 : saldo_result["saldo"]
 
         if fecha_fin.nil?
           cuenta = Cuenta.find(cuenta_id)
