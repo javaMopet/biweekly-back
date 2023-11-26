7c98e6c2b7e2d6c2d9759a60104c0a1c0b31eb16
Horacio Peña Mendoza <hpenam.dtic@gmail.com>
2023-11-25 22:22:50 -0600

    cambios

--- db/seeds/1000_menus_seed.rb
+++ db/seeds/1000_menus_seed.rb
@@ -8,35 +8,86 @@
 # end
 
 begin
-  ActiveRecord::Base.connection.execute('DELETE FROM menus;
+  ActiveRecord::Base.connection.execute(
+    'DELETE FROM menus;
     DECLARE @reseed tinyint = IIF(@@ROWCOUNT > 0, 0, 1);
     DBCC CHECKIDENT (menus, RESEED, @reseed);
-    ') # sqlserver
+    '
+  )
+# sqlserver
 rescue StandardError => e
   p e.message
   puts 'No sqlserver'
 end
 
-Menu.create!(nombre: 'Movimientos', icono: 'list', ruta: 'movimientos', padre: 0, tiene_hijos: 1,
-             orden: 100, activo: 1)
+Menu.create!(
+  nombre: 'Movimientos',
+  icono: 'list',
+  ruta: 'movimientos',
+  padre: 0,
+  tiene_hijos: 1,
+  orden: 100,
+  activo: 1
+)
 
-menu_administracion = Menu.create!(nombre: 'Administracion', icono: 'computer', ruta: 'admin',
-                                   padre: 0, tiene_hijos: 1, orden: 200, activo: 1)
+menu_administracion = Menu.create!(
+  nombre: 'Administracion',
+  icono: 'computer',
+  ruta: 'admin',
+  padre: 0,
+  tiene_hijos: 1,
+  orden: 200,
+  activo: 1
+)
 
-Menu.create!(nombre: 'Categorias', icono: 'category', ruta: 'categorias',
-             padre: menu_administracion.id, tiene_hijos: 1, orden: 300, activo: 1)
+Menu.create!(
+  nombre: 'Categorias',
+  icono: 'category',
+  ruta: 'categorias',
+  padre: menu_administracion.id,
+  tiene_hijos: 1,
+  orden: 300,
+  activo: 1
+)
 
-Menu.create!(nombre: 'Cuentas', icono: 'wallet', ruta: 'cuentas',
-             padre: 0, tiene_hijos: 1, orden: 160, activo: 1)
+Menu.create!(nombre: 'Cuentas', icono: 'wallet', ruta: 'cuentas', padre: 0, tiene_hijos: 1, orden: 140, activo: 1)
 
-Menu.create!(nombre: 'Bancos', icono: 'account_balance', ruta: 'bancos',
-             padre: menu_administracion.id, tiene_hijos: 1, orden: 320, activo: 1)
+Menu.create!(
+  nombre: 'Bancos',
+  icono: 'account_balance',
+  ruta: 'bancos',
+  padre: menu_administracion.id,
+  tiene_hijos: 1,
+  orden: 320,
+  activo: 1
+)
 
-Menu.create!(nombre: 'Cuentas Contables', icono: 'list_alt', ruta: 'cuentas_contables',
-             padre: menu_administracion.id, tiene_hijos: 1, orden: 320, activo: 1)
+Menu.create!(
+  nombre: 'Cuentas Contables',
+  icono: 'list_alt',
+  ruta: 'cuentas_contables',
+  padre: menu_administracion.id,
+  tiene_hijos: 1,
+  orden: 320,
+  activo: 1
+)
 
-Menu.create!(nombre: 'Opciones', icono: 'settings', ruta: 'system_options', padre: 0,
-             tiene_hijos: 1, orden: 600, activo: 1)
+Menu.create!(
+  nombre: 'Opciones',
+  icono: 'settings',
+  ruta: 'system_options',
+  padre: 0,
+  tiene_hijos: 1,
+  orden: 600,
+  activo: 1
+)
 
-Menu.create!(nombre: 'Tarjetas de crédito', icono: 'credit_card', ruta: 'tarjetas_credito',
-             padre: 0, tiene_hijos: 1, orden: 150, activo: 1)
+Menu.create!(
+  nombre: 'Tarjetas de crédito',
+  icono: 'credit_card',
+  ruta: 'tarjetas_credito',
+  padre: 0,
+  tiene_hijos: 1,
+  orden: 150,
+  activo: 1
+)
