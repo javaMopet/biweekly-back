ac60cb8c770111e5796049beb2c4520f84726ac5
Horacio Peña Mendoza <hpenam.dtic@gmail.com>
2023-12-14 19:01:24 -0600

    cambios

--- app/graphql/types/registro_type.rb
+++ app/graphql/types/registro_type.rb
@@ -7,13 +7,13 @@ module Types
     field :estado_registro_id, Integer, null: false
     field :tipo_afectacion, String, null: false
     field :importe, Float
-    field :importe_string, String, null: true
     field :fecha, GraphQL::Types::ISO8601Date
     field :observaciones, String
     field :nombre_completo, String
     field :cuenta_id, Integer, null: false
     field :cargo, Float, null: true
     field :abono, Float, null: true
+    field :disable, Boolean, null: false
     field :registro_tarjeta, Types::RegistroTarjetaType, null: true
     field :traspaso_detalle, Types::TraspasoDetalleType, null: true
     field :categoria, Types::Categorias::CategoriaType, null: true
@@ -22,8 +22,9 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-    def importe_string
-      object.importe.to_s
+    # bandera que determina si el registro puede tener algun cambio
+    def disable
+      !object.registro_tarjeta.nil?
     end
   end
 end
