0d88a7980b84e6200d27375e1b90928875401af1
Horacio peña Mendoza <hpenam.dtic@gmai.com>
2024-05-17 00:06:05 -0600

    Organizando los routes.rb

--- app/graphql/resolvers/cuentas/lista_cuentas.rb
+++ app/graphql/resolvers/cuentas/lista_cuentas.rb
@@ -6,9 +6,13 @@ module Resolvers
     class ListaCuentas < Resolvers::Base
       type [Types::Cuentas::CuentaType], null: false
 
+      # argument :id, ID, required: false
+
       # Default method
       def resolve
-        Cuenta.includes(:cuenta_contable, :tipo_cuenta, :banco).where(instance_id: current_user.instance_id).all
+        cuentas = Cuenta.includes(:cuenta_contable, :tipo_cuenta, :banco).where(instance_id: current_user.instance_id)
+        # cuentas = cuentas.where(id:) if id
+        cuentas.all
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,42 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # mount_graphql_devise_for User, at: 'graphql_auth'
-  # resources :tipos_cuenta_traspaso
-  # resources :traspaso_detalles
-  # resources :traspasos
-  # resources :bancos
+  # only for show 1 element y create pago arriba
   post '/create_multiple_registros_tarjeta', to: 'registros_tarjeta#create_multiple'
   post '/registros/create_multiple', to: 'registros#create_multiple'
   post '/registros_tarjeta/create_pago', to: 'registros_tarjeta#create_pago'
-  # get 'saldo_tarjeta_credito', to: 'registros_tarjeta#saldo_tarjeta_credito'
-  # only for show 1 element y create pago arriba
-  resources :registros_tarjeta
-  # resources :cortes_cuenta
-  resources :inversiones
+
   get 'columnas', to: 'registros#columnas'
   get 'to_excel', to: 'registros#excel_example'
   get 'movimientos', to: 'registros#movimientos'
   get 'saldos_cuentas', to: 'registros#saldos_cuentas'
   get 'saldos_movimientos', to: 'registros#saldos_movimientos'
   get 'saldos_finales', to: 'registros#saldos_finales'
-  # get 'to_excel', to: 'tipos_cuenta#show'
-  # resources :transferencias
-  # resources :egresos
-  # resources :ingresos
+
+  resources :registros_tarjeta
   resources :registros
-  # resources :tipos_cuenta
-  resources :tipos_movimiento
 
   # Para reportes excel xlsx
   get 'create_xls_template', to: 'tipos_cuenta#create_xls_template'
   get 'movimientos_to_excel', to: 'movimientos#to_excel'
   post 'totales_por_categoria', to: 'reportes#totales_por_categoria'
+  post '/graphql', to: 'graphql#execute'
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
 
+  # mount_graphql_devise_for User, at: 'graphql_auth'
+  # resources :tipos_cuenta_traspaso
+  # resources :traspaso_detalles
+  # resources :traspasos
+  # resources :bancos
+  # get 'saldo_tarjeta_credito', to: 'registros_tarjeta#saldo_tarjeta_credito'
+  # resources :cortes_cuenta
+  # resources :inversiones
+  # resources :tipos_movimiento
+  # get 'to_excel', to: 'tipos_cuenta#show'
+  # resources :transferencias
+  # resources :egresos
+  # resources :ingresos
+  # resources :tipos_cuenta
   # devise_for :users,
   #            controllers: {
   #              sessions: 'users/sessions',
@@ -40,14 +44,9 @@ Rails.application.routes.draw do
   #            },
   #            defaults: { format: :json }
 
-  resources :cuentas
+  # resources :cuentas
   # resources :cuentas_contable
   # resources :categorias
-
-  post '/graphql', to: 'graphql#execute'
-
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
-
   # get 'pages/home'
   # get 'member-data', to: 'members#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
