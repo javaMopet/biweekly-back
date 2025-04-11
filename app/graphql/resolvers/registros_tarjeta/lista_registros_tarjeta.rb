0b59939d712b705842441c0e5fe2103a65be05e1
Horacio Peña mendoza <hpenam.dtic@gmail.com>
2025-04-10 23:45:32 -0600

    Cambios en la forma de obtener los registros de la tarjeta

--- app/graphql/resolvers/registros_tarjeta/lista_registros_tarjeta.rb
+++ app/graphql/resolvers/registros_tarjeta/lista_registros_tarjeta.rb
@@ -6,21 +6,24 @@ module Resolvers
     class ListaRegistrosTarjeta < Resolvers::Base
       type [Types::RegistroTarjetaType], null: false
 
-      argument :cuenta_id, ID, required: false
-      argument :fecha_inicio, GraphQL::Types::ISO8601Date, required: true
-      argument :fecha_fin, GraphQL::Types::ISO8601Date, required: true
+      argument :cuenta_id, ID, required: true
+      argument :fecha_inicio, GraphQL::Types::ISO8601Date, required: false
+      argument :fecha_fin, GraphQL::Types::ISO8601Date, required: false
       argument :is_msi, Boolean, required: false
       argument :estado_registro_tarjeta_id, ID, required: false
 
       # Metodo resolver
-      def resolve(cuenta_id:, fecha_inicio:, fecha_fin:, is_msi:, estado_registro_tarjeta_id:)
+      def resolve(cuenta_id:, is_msi:, estado_registro_tarjeta_id:, fecha_inicio: nil, fecha_fin: nil)
         # sleep(2)
-        registros = RegistroTarjeta.includes(:estado_registro_tarjeta, :cuenta, :categoria)
-        registros = registros.where(cuenta_id:) if cuenta_id
+        return [] if fecha_inicio.nil? || fecha_fin.nil?
+
+        return [] if fecha_inicio > fecha_fin
+
+        registros = RegistroTarjeta.includes(:estado_registro_tarjeta, :cuenta, :categoria).where(cuenta_id:)
         registros = registros.where(estado_registro_tarjeta_id:) if estado_registro_tarjeta_id
         registros = registros.where(fecha: fecha_inicio..fecha_fin)
         registros = registros.where(is_msi:) unless is_msi.nil?
-        registros.order(fecha: :asc, id: :desc).all
+        registros.order(fecha: :asc, id: :desc)
       end
     end
   end
